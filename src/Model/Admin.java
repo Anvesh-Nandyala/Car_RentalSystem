9d9990bc1588138eb33bff7d7b43bde962206673
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-06-23 23:55:58 +0530

    fixed minor bugs

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -14,12 +14,13 @@ public class Admin extends User {
     public void showList(Database database, Scanner s) {
         System.out.println("\n1. Add New Model Car");
         System.out.println("2. View Cars");
-        System.out.println("3. Update Model Car");
-        System.out.println("4. Delete Model Car");
-        System.out.println("5. Add New Model Admin");
+        System.out.println("3. Update Car");
+        System.out.println("4. Delete Car");
+        System.out.println("5. Add New Admin");
         System.out.println("6. Show Rents");
         System.out.println("7. Quit\n");
          int i=s.nextInt();
          operations[0].operation(database, s, this);
+         showList(database,s);
     }
 }
--- src/Model/Client.java
+++ src/Model/Client.java
@@ -10,8 +10,8 @@ public class Client extends User {
     @Override
     public void showList(Database database, Scanner s){
         System.out.println("\n1. View Cars");
-        System.out.println("2. Model.Rent Model.Car");
-        System.out.println("3. Return Model.Car");
+        System.out.println("2. Rent Car");
+        System.out.println("3. Return Car");
         System.out.println("4. Show My Rents");
         System.out.println("5. Edit My Data");
         System.out.println("6. Quit\n");
