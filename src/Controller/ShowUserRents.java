c34b07980512258e3f6cc94602ce44cce00760c0
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-06-25 01:49:51 +0530

    fixed bug in rentcar, and added  a feature to admin to view all rents of clients

--- src/Controller/ShowUserRents.java
+++ src/Controller/ShowUserRents.java
@@ -38,45 +38,48 @@ public class ShowUserRents implements Operation {
             String selectUser = "SELECT * FROM public.\"carRental\" WHERE \"ID\" = " + userID;
 
             ResultSet rs2=database.getStatement().executeQuery(selectUser);
-            rs2.next();
-            User u=new Client();
-            u.setID(rs2.getInt("ID"));
-            u.setFirstName(rs2.getString("FirstName"));
-            u.setLastName(rs2.getString("LastName"));
-            u.setEmail(rs2.getString("Email"));
-            u.setPhoneNumber(rs2.getString("PhoneNumber"));
-            u.setPassword((rs2.getString("Password")));
-
-            for(int j=0;j<rents.size();j++){
-                Rent r=rents.get(j);
-                r.setUser(u);
-                ResultSet rs3=database.getStatement()
-                        .executeQuery("SELECT * FROM public.cars WHERE \"ID\" ="+ carIDs.get(j));
-                rs3.next();
-                Car car=new Car();
-                car.setID(rs3.getInt("ID"));
-                car.setBrand(rs3.getString("brand"));
-                car.setModel(rs3.getString("model"));
-                car.setYear(rs3.getInt("year"));
-                car.setPrice(rs3.getDouble("price"));
-                car.setColor(rs3.getString("color"));
-                car.setAvailable(rs3.getInt("available"));
-                r.setCar(car);
-
-                System.out.println("ID:\t"+r.getID());
-                System.out.println("Name:\t"+r.getUser().getFirstName()+" "+ r.getUser().getLastName());
-                System.out.println("Email:\t"+r.getUser().getEmail());
-                System.out.println("Phone Number:\t"+r.getUser().getPhoneNumber());
-                System.out.println("Car ID:\t"+r.getCar().getId());
-                System.out.println("Car:\t"+r.getCar().getBrand()+" "+r.getCar().getModel()+" "+r.getCar().getColor());
-                System.out.println("Date Time:\t"+r.getDateTime());
-                System.out.println("Hours:\t"+r.getHours());
-                System.out.println("Total:\t"+r.getTotal());
-                System.out.println("Status:\t"+r.getStatusToString());
-                System.out.println("-------------------\n");
+            if(rs2.next()) {
+                User u = new Client();
+                u.setID(rs2.getInt("ID"));
+                u.setFirstName(rs2.getString("FirstName"));
+                u.setLastName(rs2.getString("LastName"));
+                u.setEmail(rs2.getString("Email"));
+                u.setPhoneNumber(rs2.getString("PhoneNumber"));
+                u.setPassword((rs2.getString("Password")));
 
 
+                for(int j=0;j<rents.size();j++) {
+                    Rent r = rents.get(j);
+                    r.setUser(u);
+                    ResultSet rs3 = database.getStatement()
+                            .executeQuery("SELECT * FROM public.cars WHERE \"ID\" =" + carIDs.get(j));
+                    rs3.next();
+                    Car car = new Car();
+                    car.setID(rs3.getInt("ID"));
+                    car.setBrand(rs3.getString("brand"));
+                    car.setModel(rs3.getString("model"));
+                    car.setYear(rs3.getInt("year"));
+                    car.setPrice(rs3.getDouble("price"));
+                    car.setColor(rs3.getString("color"));
+                    car.setAvailable(rs3.getInt("available"));
+                    r.setCar(car);
 
+                    System.out.println("ID:\t" + r.getID());
+                    System.out.println("Name:\t" + r.getUser().getFirstName() + " " + r.getUser().getLastName());
+                    System.out.println("Email:\t" + r.getUser().getEmail());
+                    System.out.println("Phone Number:\t" + r.getUser().getPhoneNumber());
+                    System.out.println("Car ID:\t" + r.getCar().getId());
+                    System.out.println("Car:\t" + r.getCar().getBrand() + " " + r.getCar().getModel() + " " + r.getCar().getColor());
+                    System.out.println("Date Time:\t" + r.getDateTime());
+                    System.out.println("Hours:\t" + r.getHours());
+                    System.out.println("Total:\t" + r.getTotal());
+                    System.out.println("Status:\t" + r.getStatusToString());
+                    System.out.println("-------------------\n");
+                }
+
+
+            }else{
+                System.out.println("User with ID " + userID + " not found.");
             }
         } catch (SQLException e){
             e.printStackTrace();
