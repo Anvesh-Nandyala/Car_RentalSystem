17c8f9e4ef9587bb8d2483a99b40ee63670de1c3
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-06-24 22:00:26 +0530

    added options edituserdata and quit

---
+++ src/Controller/EditUserData.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import Model.Database;
+import Model.Operation;
+import Model.User;
+
+import java.util.Scanner;
+
+public class EditUserData implements Operation {
+    @Override
+    public void operation(Database database, Scanner s, User user) {
+
+    }
+}
